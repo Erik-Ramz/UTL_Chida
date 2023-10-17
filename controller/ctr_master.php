9b953a895237e5eb4e522abc5aeae893f40cacb1
Erik Ramirez <danielrmzmares@gmail.com>
2023-10-17 15:17:51 -0600

    Estructuración del controlador

--- controller/ctr_master.php
+++ controller/ctr_master.php
@@ -1,3 +1,14 @@
 <?php
-    $peticion="index";
+  $peticion="home";
+  extract($_REQUEST);
+  switch($peticion){
+    case "home":
+        break;
+    case "...":
+        break;
+    default:
+    header('location:?peticion=home');
+        break;
+  }
+
 ?>
