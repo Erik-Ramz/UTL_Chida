5d0ae6ec72185746cd359da777e25dfc432651b5
Erik Ramirez <danielrmzmares@gmail.com>
2023-11-07 16:50:45 -0600

    Actualizando los cambios

--- components/navbar.php
+++ components/navbar.php
@@ -12,12 +12,13 @@
                 </label>
                 <div class="chocolate-menu">
                     <ul>
-                        <li><a href="https://sito.utleon.edu.mx/" target="_blank"><i class='bx bx-windows'></i>SITO</a></li>
-                        <li><a href="https://aulavirtual.seg.guanajuato.gob.mx/materias/login/index.php" target="_blank"><i class='bx bx-world'></i>Aula Virtual</a></li>
-                        <li><a href="https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=16&ct=1698001631&rver=7.0.6738.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fcobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26nlp%3d1%26deeplink%3dowa%252f%26RpsCsrfState%3ddbd63f50-097d-4926-47b1-d027c06f7cee&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c" target="_blank"><i class='bx bx-mail-send'></i>Outlook</a></li>
-                        <li><a href="http://www.bibliotecaecest.mx/" target="_blank"><i class='bx bx-book-bookmark'></i>Biblioteca</a></li>
-                        <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N__.jpg" target="_blank"><i class='bx bx-calendar'></i>2022 - 2023</a></li>
-                        <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N.jpg" target="_blank"><i class='bx bx-calendar'></i>2023 - 2024</a></li>
+                        <li><a href="https://sito.utleon.edu.mx/" target="_blank"><img src="../../svg/sito.svg" alt="">SITO</a></li>
+                        <li><a href="https://aulavirtual.seg.guanajuato.gob.mx/materias/login/index.php" target="_blank"><img src="../../svg/aula-virtual.svg" alt="">Aula Virtual</a></li>
+                        <li><a href="https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=16&ct=1698001631&rver=7.0.6738.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fcobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26nlp%3d1%26deeplink%3dowa%252f%26RpsCsrfState%3ddbd63f50-097d-4926-47b1-d027c06f7cee&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c" target="_blank"><img src="../../svg/outlook.svg" alt="">Outlook</a></li>
+                        <li><a href="http://www.bibliotecaecest.mx/" target="_blank"><img src="../../svg/books.svg" alt="">Biblioteca</a></li>
+                        <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N__.jpg" target="_blank"><img src="../../svg/calendar.svg" alt="">2022 - 2023</a></li>
+                        <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N.jpg" target="_blank"><img src="../../svg/calendar.svg" alt="">2023 - 2024</a></li>
+                        <li><a href="https://www.utleon.edu.mx/public_html/transparencia/normateca/docs/normas/reglamento_academico.pdf" target="_blank"><img src="../../svg/rules.svg" alt="">Reglamento</a></li>
                     </ul>
                 </div>
             </div>
@@ -27,7 +28,7 @@
     <div class="navbar-container">
         <ul class="navbar-menu">
             <li class="dropdown-utl">
-                <a href="../../index.php">Inicio</a>
+                <a href="../../index.php">Zona UTL</a>
             </li>
             <li class="dropdown-utl">
                 <a href="#">Aspirantes <i class='bx bx-chevron-down'></i></a>
@@ -53,9 +54,9 @@
             <li class="dropdown-utl">
                 <a href="#">Docentes <i class='bx bx-chevron-down'></i></a>
                 <ul class="dropdown-menu-utl">
-                    <li><a href="#">Desarrollo académico</a></li>
-                    <li><a href="#">Tutoreo</a></li>
-                    <li><a href="#">Movilidad Docente</a></li>
+                    <li><a href="../docentes/desarrollo-academico.php">Desarrollo académico</a></li>
+                    <li><a href="../docentes/tutoreo.php">Tutoreo</a></li>
+                    <li><a href="../docentes/movilidad-utl.php">Movilidad Docente</a></li>
                 </ul>
             </li>
         </ul>
