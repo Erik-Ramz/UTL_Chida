529cd3b9d7c9dce6f5d256c8ac374420853e3f4a
Erik Ramirez <danielrmzmares@gmail.com>
2023-11-07 17:04:40 -0600

    Actualización del Index

--- index.php
+++ index.php
@@ -45,12 +45,13 @@ $footer = "components/footer.php";
                     </label>
                     <div class="chocolate-menu">
                         <ul>
-                            <li><a href="https://sito.utleon.edu.mx/" target="_blank"><i class='bx bx-windows'></i>SITO</a></li>
-                            <li><a href="https://aulavirtual.seg.guanajuato.gob.mx/materias/login/index.php" target="_blank"><i class='bx bx-world'></i>Aula Virtual</a></li>
-                            <li><a href="https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=16&ct=1698001631&rver=7.0.6738.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fcobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26nlp%3d1%26deeplink%3dowa%252f%26RpsCsrfState%3ddbd63f50-097d-4926-47b1-d027c06f7cee&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c" target="_blank"><i class='bx bx-mail-send'></i>Outlook</a></li>
-                            <li><a href="http://www.bibliotecaecest.mx/" target="_blank"><i class='bx bx-book-bookmark'></i>Biblioteca</a></li>
-                            <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N__.jpg" target="_blank"><i class='bx bx-calendar'></i>2022 - 2023</a></li>
-                            <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N.jpg" target="_blank"><i class='bx bx-calendar'></i>2023 - 2024</a></li>
+                            <li><a href="https://sito.utleon.edu.mx/" target="_blank"><img src="svg/sito.svg" alt="">SITO</a></li>
+                            <li><a href="https://aulavirtual.seg.guanajuato.gob.mx/materias/login/index.php" target="_blank"><img src="svg/aula-virtual.svg" alt="">Aula Virtual</a></li>
+                            <li><a href="https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=16&ct=1698001631&rver=7.0.6738.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fcobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26nlp%3d1%26deeplink%3dowa%252f%26RpsCsrfState%3ddbd63f50-097d-4926-47b1-d027c06f7cee&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c" target="_blank"><img src="svg/outlook.svg" alt="">Outlook</a></li>
+                            <li><a href="http://www.bibliotecaecest.mx/" target="_blank"><img src="svg/books.svg" alt="">Biblioteca</a></li>
+                            <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N__.jpg" target="_blank"><img src="svg/calendar.svg" alt="">2022 - 2023</a></li>
+                            <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N.jpg" target="_blank"><img src="svg/calendar.svg" alt="">2023 - 2024</a></li>
+                            <li><a href="https://www.utleon.edu.mx/public_html/transparencia/normateca/docs/normas/reglamento_academico.pdf" target="_blank"><img src="svg/rules.svg" alt="">Reglamento</a></li>
                         </ul>
                     </div>
                 </div>
@@ -60,7 +61,7 @@ $footer = "components/footer.php";
         <div class="navbar-container">
             <ul class="navbar-menu">
                 <li class="dropdown-utl">
-                    <a href="index.php">Inicio</a>
+                    <a href="index.php">Zona UTL</a>
                 </li>
                 <li class="dropdown-utl">
                     <a href="#">Aspirantes <i class='bx bx-chevron-down'></i></a>
@@ -86,9 +87,9 @@ $footer = "components/footer.php";
                 <li class="dropdown-utl">
                     <a href="#">Docentes <i class='bx bx-chevron-down'></i></a>
                     <ul class="dropdown-menu-utl">
-                        <li><a href="#">Desarrollo académico</a></li>
-                        <li><a href="#">Tutoreo</a></li>
-                        <li><a href="#">Movilidad Docente</a></li>
+                        <li><a href="view/docentes/desarrollo-academico.php">Desarrollo académico</a></li>
+                        <li><a href="view/docentes/tutoreo.php">Tutoreo</a></li>
+                        <li><a href="view/docentes/movilidad-utl.php">Movilidad Docente</a></li>
                     </ul>
                 </li>
             </ul>
@@ -137,9 +138,10 @@ $footer = "components/footer.php";
             </div>
         </section>
 
+        <h2 class="primary-title">Modelo educativo</h2>
         <section class="modelo-estudios">
             <div class="modelo-cuatrimestres">
-                <h4 class="modelo-title">TSU</h4>
+                <h4 class="modelo-title">Técnico Superior Universitario</h4>
                 <div class="modelo-tsu">
                     <div>
                         <h5 class="modelo-turno">Matutino</h5>
@@ -155,7 +157,7 @@ $footer = "components/footer.php";
                 </div>
 
                 <div class="modelo-ing-lic">
-                    <h4 class="modelo-title">ING/LIC</h4>
+                    <h4 class="modelo-title">Ingeniería/Licenciatura</h4>
                     <p class="cuatrimestres-ing-lic">5</p>
                     <span>Cuatrimestres</span>
                 </div>
@@ -178,7 +180,7 @@ $footer = "components/footer.php";
         <section class="pilares-institucionales">
             <div class="container_utl">
                 <div class="image-box_utl">
-                    <img src="src/img/utl_5.webp" alt="Imagen de la empresa">
+                    <img src="src/img/utl_5.webp" loading="lazy" alt="Imagen de la empresa">
                 </div>
                 <div class="content-box_utl">
                     <div class="content-box_utl_info">
